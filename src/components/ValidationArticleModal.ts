32dc9f80ec5d0d885761324cb56269789c73311d
Alerrando <alerrando2@gmail.com>
2025-04-10 18:07:47 -0300

    fix: fixing bug in img file empty in add artcile

--- src/components/ValidationArticleModal.ts
+++ src/components/ValidationArticleModal.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const schemaArticleModal = z.object({
   title: z.string().min(1, "Titulo é obrigatorio"),
   summary: z.string(),
-  image: z.string(),
+  imageUrl: z.string(),
 });
 
 export type SchemaArticleModal = z.infer<typeof schemaArticleModal>;
 
-export const defaultValuesSectionModal: SchemaArticleModal = { title: "", summary: "", image: "" };
+export const defaultValuesSectionModal: SchemaArticleModal = { title: "", summary: "", imageUrl: "" };
